83f6e20eed2e60500037e37ffa8f2502afb6aa88
Tikava <wavoleniu@gmail.com>
2025-07-31 15:11:06 +0500

    feat: add dynamic application port configuration based on environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.app import app
+from src.config import settings
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=settings.app_port)
